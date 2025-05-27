450a4b92f141d04d0a8ffbece1c6c7892f1d980a
Gustavo <gustavolimasilva.developer@gmail.com>
2025-05-27 14:40:50 -0300

    if conditions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 fun main() {
     print("Please enter a number: ")
     val input = readln()
-    val inputAsInteger = input.toIntOrNull()?.rem(2)?.equals(0)
+    val inputAsInteger = input.toIntOrNull()
 
-    println("Is even? $inputAsInteger")
+    if (inputAsInteger != null) {
+        val output = if (inputAsInteger % 2 == 0) {
+            "The number is even"
+        } else if (inputAsInteger < 10) {
+            "The number is odd and less than 10"
+        } else {
+            "The number is odd and at least 11"
+        }
+        println(output)
+    } else {
+        println("Dude, enter a valid number!")
+    }
 }
