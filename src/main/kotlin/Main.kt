569f1c30c8a96bbb4c4abc8cce664b1b605c7370
Gustavo <gustavolimasilva.developer@gmail.com>
2025-05-27 07:45:48 -0300

    logical operators

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 fun main() {
     val x = 12
-    println("Is x an enven number? ${x % 2 == 0}")
+    val y = 11
+    val areBothEven = x % 2 == 0 || (y % 2 == 0 && x + y == 25)
+    println("Is x an enven number? $areBothEven")
 }
