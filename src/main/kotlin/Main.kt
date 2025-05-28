8e2f4deaf1e3d8644e92fb3e3bb4102f45284e9e
Gustavo <gustavolimasilva.developer@gmail.com>
2025-05-28 15:29:26 -0300

    loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
+    println("Enter a string: ")
     val input = readln()
-    val inputAsInteger = input.toIntOrNull()
-    val favoriteNumbers = intArrayOf(1, 2, 3, 69) + 4
-    favoriteNumbers[2] = 69
 
-    if (inputAsInteger != null && inputAsInteger in 0..favoriteNumbers.lastIndex) {
-        println("Your number is ${favoriteNumbers[inputAsInteger]}")
-    } else {
-        println("That index doesn't exist")
+    val finalString = buildString {
+        for (i in input.lastIndex downTo 0){
+            append(input[i])
+        }
     }
+
+    println(finalString)
 }
