18377bb1aadc18bdb38478041e1a46ac5a7a796a
Gustavo <gustavolimasilva.developer@gmail.com>
2025-05-28 17:12:57 -0300

    normal functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,21 @@ fun main() {
     println("Enter a string: ")
     val input = readln()
 
+    val reversed = reversed(stringToReverse = input)
+
+    println(reversed)
+
+    if (input == reversed) {
+        println("That's a palindrome!")
+    }
+}
+
+fun reversed(stringToReverse: String): String {
     val finalString = buildString {
-        for (i in input.lastIndex downTo 0){
-            append(input[i])
+        for (i in stringToReverse.lastIndex downTo 0) {
+            append(stringToReverse[i])
         }
     }
 
-    println(finalString)
+    return finalString
 }
