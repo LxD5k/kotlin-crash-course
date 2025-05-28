bc0875fad2636ab7365be9933519b74adab9cfd8
Gustavo <gustavolimasilva.developer@gmail.com>
2025-05-28 07:15:07 -0300

    exceptions & try/catch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 fun main() {
     print("Please enter a number: ")
     val input = readln()
-    val inputAsInteger = input.toIntOrNull()
+    val inputAsInteger = try{
+        input.toInt()
+    } catch (e: NumberFormatException){
+        0
+    }
 
     val output = when (inputAsInteger) {
-        null -> "Dude, enter a valid number"
         3 -> "The number is three!"
         5 -> "The number is five!"
         in 10..20 -> "the number is between 10 and 20"
