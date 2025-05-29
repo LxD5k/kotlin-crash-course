24e0a02ffeec248f90f6834ac2c38c9068544f3f
Gustavo <gustavolimasilva.developer@gmail.com>
2025-05-29 16:38:41 -0300

    interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,23 +11,37 @@ fun main() {
         radius = 5f,
     )
 
-    println(circle.area)
-    println("The area is ${rect1.area}. The diagonal is ${rect1.diagonal}")
+    println(sumAreas(rect1, circle))
+}
+
+fun sumAreas(vararg shapes: Shape): Double {
+    return shapes.sumOf { currentShape ->
+        currentShape.area.toDouble()
+    }
+}
+
+interface Shape {
+    val area: Float
+    val circumference: Float
 }
 
 data class Rectangle(
     val width: Float,
     val height: Float,
-) {
+) : Shape {
     val diagonal = sqrt(width.pow(2) + height.pow(2))
 
-    val area = width * height
+    override val area = width * height
+
+    override val circumference: Float = width.pow(2) + height.pow(2)
 }
 
 data class Circle(
     val radius: Float
-) {
-    val area = radius.pow(2) * PI
+) : Shape {
+    override val area = radius.pow(2) * PI.toFloat()
 
     val diameter = radius.pow(2)
+
+    override val circumference: Float = radius.pow(2) * PI.toFloat()
 }
